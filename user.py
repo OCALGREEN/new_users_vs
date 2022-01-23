17e6d62de032f4b949ddfb9b6ea46b38e44ee39e
Oger Carrillo <ogercarrillo@gmail.com>
2022-01-23 02:51:21 -0800

    crud

---
+++ user.py
@@ -0,0 +1,35 @@
+from unittest import result
+from winreg import QueryInfoKey
+from mysqlconnection import connectToMySQL
+
+class User:
+    def __init__(self, data):
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.email = data["email"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
+# CREATE
+    @classmethod
+    def get_all(cls): # function to receive data from database
+        query = "SELECT * FROM users;"   # this is an id but it is also the database statement
+        results = connectToMySQL("new_users").query_db(query)   # not sure what this is again
+        users = []  # an empty to list to add all the data from the database
+        for user in results:  # will iterate through the info received from db and add it to the empty list
+            users.append(cls(user))
+        return users # returns the populated list
+
+# READ ALL
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES (%(fname)s, %(lname)s, %(nemail)s, NOW(), NOW());"
+        return connectToMySQL("new_users").query_db(query, data)
+
+# READ ONE
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        results = connectToMySQL("new_users").query_db(query, data)
+        return cls(results[0])
