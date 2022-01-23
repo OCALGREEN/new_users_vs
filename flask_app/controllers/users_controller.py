d5c16cd03d788a89314264a8d8a9bc13ffd36cd7
Oger Carrillo <ogercarrillo@gmail.com>
2022-01-23 15:43:32 -0800

    modularized

---
+++ flask_app/controllers/users_controller.py
@@ -0,0 +1,54 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash 
+from flask_app.model.user import User 
+
+# HOME PAGE
+@app.route("/")
+def index():
+    users = User.get_all() # calling the get_all() function from the class/model to output the information onto the html
+    print(users) # prints info onto the terminal for trouble shooting
+    return render_template("index.html", users = users)
+
+
+# CREATE PAGE ADD NEW USER
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+# WILL REDIRECT TO HOME PAGE WITH NEW USER CREATED
+@app.route("/create/new_user", methods=["POST"])
+def create_new_user():
+    data = {
+        "fname": request.form["fname"], # we make a dictionary from our request.form coming fromt the template
+        "lname": request.form["lname"], # the keys in the data need to line up exactly with the variables in our query string
+        "nemail": request.form["nemail"],
+    }
+    User.save(data) # we pass the data dictionary into the save data method from the class
+    return redirect("/")
+
+# WILL SHOW THE SELECTED PERSONS INFORMATION
+@app.route("/show/<int:user_id>")
+def show(user_id):
+    return render_template("show.html", this_user = User.get_one({"id": user_id}))
+
+# EDIT PAGE
+@app.route("/edit/<int:user_id>")
+def edit(user_id):
+    return render_template("edit.html", this_user = User.get_one({"id": user_id}))
+
+# EDIT PAGE REDIRECT
+@app.route("/edit/<int:user_id>/update", methods = ["POST"])
+def edit_user(user_id):
+    updated_data = {
+        **request.form, 
+        "id": user_id
+    }
+    User.update(updated_data)
+    return redirect("/")
+
+# DELETE USER
+@app.route("/destroy/<int:user_id>")
+def destroy(user_id):
+    data = {"id": user_id}
+    User.destroy(data)
+    return redirect("/")
